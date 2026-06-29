7348e8adc5f8b33d1db74ec0a87ea63fef0a13e4
david0501z <zhangdw0501@outlook.com>
2026-06-29 21:05:17 +0800

    add getActivity() for ProgressDialog

--- app/src/main/java/com/github/catvod/spider/Init.java
+++ app/src/main/java/com/github/catvod/spider/Init.java
@@ -1,10 +1,15 @@
 package com.github.catvod.spider;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 
+import com.github.catvod.crawler.SpiderDebug;
+
+import java.lang.reflect.Field;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -46,5 +51,26 @@ public class Init {
 
     public static void post(Runnable runnable, int delay) {
         get().handler.postDelayed(runnable, delay);
+    }
+
+    public static Activity getActivity() throws Exception {
+        Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
+        Object activityThread = activityThreadClass.getMethod("currentActivityThread").invoke(null);
+        Field activitiesField = activityThreadClass.getDeclaredField("mActivities");
+        activitiesField.setAccessible(true);
+        Map<?, ?> activities = (Map<?, ?>) activitiesField.get(activityThread);
+        for (Object activityRecord : activities.values()) {
+            Class<?> activityRecordClass = activityRecord.getClass();
+            Field pausedField = activityRecordClass.getDeclaredField("paused");
+            pausedField.setAccessible(true);
+            if (!pausedField.getBoolean(activityRecord)) {
+                Field activityField = activityRecordClass.getDeclaredField("activity");
+                activityField.setAccessible(true);
+                Activity activity = (Activity) activityField.get(activityRecord);
+                SpiderDebug.log(activity.getComponentName().getClassName());
+                return activity;
+            }
+        }
+        return null;
     }
 }
